e59e8cdc55043717d23c7dc5cb6ed542d457f550
KOne <55965735+k-f-m@users.noreply.github.com>
2024-01-30 10:50:15 +0100

    Configure proxy to mock server

--- cardifyit-client/vite.config.js
+++ cardifyit-client/vite.config.js
@@ -5,6 +5,25 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,  // Client port
-  }
-})
+    // Client port
+    port: 3000,
+    proxy: {
+      '/cards': {
+        // Mock server port
+        target: 'http://localhost:5200',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        onProxyReq: (proxyReq, req, _res) => {
+          console.log('Sending request to the target:', req.method, req.url);
+        },
+        onProxyRes: (proxyRes, req, _res) => {
+          console.log('Received response from the target:', proxyRes.statusCode, req.url);
+        },
+        onError: (err, _req, _res) => {
+          console.error('Proxy error:', err);
+        },
+      },
+    },
+  },
+});
