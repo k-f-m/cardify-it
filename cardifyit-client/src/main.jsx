eb3b639cffff4a689e3377c7ef7b0be8326d63c6
KOne <55965735+k-f-m@users.noreply.github.com>
2024-01-31 00:18:19 +0100

    Set up Material UI

--- cardifyit-client/src/main.jsx
+++ cardifyit-client/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+const theme = createTheme();
+
+import Card from './Card'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Card className="Card"/>
+    </ThemeProvider>
   </React.StrictMode>,
 )
