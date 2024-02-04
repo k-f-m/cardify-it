d5a81c01ff0450867b808421714cd3be7b59ce33
KOne <55965735+k-f-m@users.noreply.github.com>
2024-02-04 01:04:37 +0100

    Add Swagger

--- cardifyit-api/Program.cs
+++ cardifyit-api/Program.cs
@@ -1,5 +1,19 @@
+using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cardify It! API", Description = "Turn JSON data into product cards.", Version = "v1" });
+});
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+   c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+});
 
 app.MapGet("/", () => "Hello World!");
 
