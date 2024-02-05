6aa837d6ed1850ed4d9f558b3334fed1f7376f48
KOne <55965735+k-f-m@users.noreply.github.com>
2024-02-06 00:19:10 +0100

    Configure CORS policy
    This commit configures cross-origin resource sharing (CORS) so that the front-end app and API can communicate despite running on different ports.

--- cardifyit-api/Program.cs
+++ cardifyit-api/Program.cs
@@ -7,6 +7,20 @@ builder.Services.AddSwaggerGen(c =>
      c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cardify It! API", Description = "Turn JSON data into product cards.", Version = "v1" });
 });
 
+// Define a unique identifier for CORS policy.
+string allowSpecificOrigins = "_allowSpecificOrigins";
+
+// Define allowed domains.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: allowSpecificOrigins,
+      builder =>
+      {
+          builder.WithOrigins(
+            "http://localhost");
+      });
+});
+
 var app = builder.Build();
 
 app.UseSwagger();
@@ -14,6 +28,8 @@ app.UseSwaggerUI(c =>
 {
    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
 });
+
+app.UseCors(allowSpecificOrigins);
 
 app.MapGet("/", () => "Hello World!");
 
