7b63b1a0b719b36c988058565210880b95947dde
KOne <55965735+k-f-m@users.noreply.github.com>
2024-02-10 23:49:39 +0100

    Add entity and context classes
    This commit adds the EF core model for a SQLight database to be used instead of the in-memory database.

---
+++ cardifyit-api/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace cardifyit_api.Models
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? ProductUrl { get; set; }
+        public int? AddToCardId { get; set; }
+    }
+}
---
+++ cardifyit-api/Models/ProductsDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace cardifyit_api.Models
+{
+    public class ProductsDbContext : DbContext
+    {
+        public ProductsDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Product> Products { get; set; } = null!;
+    }
+}
